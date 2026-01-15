65380c7b44c476cb05371cbbde297815eaba4250
xumingyue <xumingyue@rd.netease.com>
2026-01-15 20:49:37 +0800

    fix(deploy): remove residual OpenNext config files causing build failure

--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-    // Default configuration
-    // incrementalCache can be configured here if using R2
-});
